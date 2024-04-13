a1ce79051b53e4baaf5ae4f9aaf3fb46538fbecd
Phillipe Duarte Gori <pdgori@hotmail.com>
2024-04-13 19:39:31 -0300

    End of section 10 - Handling validation error

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 using Application.Activities;
 using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -38,6 +40,9 @@ namespace API.Extensions
                 cfg.RegisterServicesFromAssemblies(typeof(List.Handler).Assembly)
             );
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
 
             return services;
         }
